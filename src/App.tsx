b603ce4bb6fa9d0600932ea5b51b82b9b4a65e81
andreibaicus <baicusandrei@me.com>
2022-08-18 10:33:53 +0300

    chore: add code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
+import IsolatedBlockEditor from '@automattic/isolated-block-editor';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+  const defaultCallback = () => {
+    console.log('default');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* @ts-ignore */}
+      <IsolatedBlockEditor
+        settings={{}}
+        onSaveBlocks={defaultCallback}
+        onSaveContent={defaultCallback}
+        onError={defaultCallback}
+      />
     </div>
   );
 }
 
 export default App;
+
+/**
+ * 
+ * @param {Object} props - Component props
+ * @param {OnSaveBlocks} [props.onSaveBlocks] - Save callback
+ * @param {OnSaveContent} [props.onSaveContent] - Save callback
+ * @param {OnError} props.onError - Error callback
+ * @param {OnLoad} [props.onLoad] - Initial blocks
+ * @param {BlockEditorSettings} props.settings - Settings
+ * @param {Object} [props.children] - Child content
+ * @param {string} [props.className] - Additional class name
+ * @param {OnMore} [props.renderMoreMenu] - Callback to render additional items in the more menu
+ * @param {UndoManager} [props.__experimentalUndoManager] - Undo manager
+ * @param {OnUpdate} [props.__experimentalOnInput] - Gutenberg's onInput callback
+ * @param {OnUpdate} [props.__experimentalOnChange] - Gutenberg's onChange callback
+ * @param {OnSelect} [props.__experimentalOnSelection] - Callback to run when the editor selection changes
+ * @param {object[]} [props.__experimentalValue] - Gutenberg's value
+ * 
+ */
